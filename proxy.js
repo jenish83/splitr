87a2119d59beb898b8a35d0e901d5101c1a39084
Jenish Panchasara <jenishpanchasara83@gmail.com>
2026-05-28 19:50:40 +0530

    Design Landing page

--- proxy.js
+++ proxy.js
@@ -1,33 +1,29 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
-import { createRouteMatcher } from 'next/navigation';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/expenses(.*)',
-  '/contacts(.*)',
-  '/groups(.*)',
-  '/persom(.*)',
-  '/settlements(.*)',
+  "/dashboard(.*)",
+  "/expenses(.*)",
+  "/contacts(.*)",
+  "/groups(.*)",
+  "/persom(.*)", // or "/person(.*)" if that was a typo
+  "/settlements(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
-  const { userId } = await auth();
+  const { userId, redirectToSignIn } = await auth();
 
-  if(!userId && isProtectedRoute(request)) {
-    const {redirectToSignIn} = await auth();
+  if (!userId && isProtectedRoute(request)) {
     return redirectToSignIn();
   }
+
   return NextResponse.next();
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-    // Always run for Clerk-specific frontend API routes
-    '/__clerk/(.*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+    "/__clerk/(.*)",
   ],
-}
+};
